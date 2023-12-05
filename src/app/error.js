6f79055bf20a3223d059a24523da53c0fc6883ea
rafimd422 <rafimf2222@gmail.com>
2023-12-06 01:00:40 +0600

    created the error page to show a different interface whenever there is an error occurs

---
+++ src/app/error.js
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+    return (
+      <div className='h-screen flex justify-center items-center'>
+        <h1>Error 404 !! Something Went Wrong!</h1>
+      </div>
+    )
+  }
+  
+  export default ErrorPage
+  
