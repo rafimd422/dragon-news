b1be809a6bde55983a46b21df3ecd03611b11845
rafimd422 <rafimf2222@gmail.com>
2023-12-05 22:39:32 +0600

    created a card and set up on the sidebar top

--- src/components/ui/LatestNews/Sidebar/Sidebar.js
+++ src/components/ui/LatestNews/Sidebar/Sidebar.js
@@ -1,14 +1,15 @@
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
 import Image from 'next/image'
+import sidebar1 from '@/assets/sidebar1.png'
 
 const Sidebar = () => {
   return (
-    <Box>
+    <Box marginTop={'5px'}>
       <Card>
       <CardActionArea>
         <CardMedia>
           <Image
-          src={''}
+          src={sidebar1}
           alt="top news"
           width={800}
           />
@@ -16,14 +17,11 @@ const Sidebar = () => {
         <p className="w-[100px] ms-2 bg-red-500 text-white p-2 my-1 rounded">Technology</p>
         <CardContent>
         <Typography gutterBottom fontWeight={'bold'} component="div">
-            Bitcoin climbs as Elon Musk Says Tesla Likely to accept it Again
+        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
           </Typography>
-          <Typography gutterBottom className="my-3">
-By Tanmoy Parvez - Mar 18 2023</Typography>
-
-<Typography variant="body2" color={'text.secondary'}>
-It is a long established fact that a reader will be distracted by the readable content of a page when looging at its layout.........
-</Typography>
+          <Typography gutterBottom fontSize={'.8rem'} className="my-3">
+          By Awlad Hossain - Mar 18 2023
+          </Typography>
                </CardContent>
       </CardActionArea>
     </Card>
